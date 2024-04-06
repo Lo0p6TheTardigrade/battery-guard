53ee4ebf2e879b33341a2bd8210f9cbed192dde9
Lo0p6TheTardigrade <rezonance30@gmail.com>
2024-04-06 04:40:22 +0200

    Set a temporary navigation

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
-  return <nav className="nav-container">Navigation</nav>;
+  return (
+    <nav className="nav-container">
+      Navigation{' '}
+      <ul className="nav-list-container">
+        <Link to="/">
+          <li className="nav-list-item">Home</li>
+        </Link>
+        <Link to="/my-lipo">
+          <li className="nav-list-item">My Lipo</li>
+        </Link>
+        <Link to="/help">
+          <li className="nav-list-item">Help</li>
+        </Link>
+        <Link to="/about">
+          <li className="nav-list-item">About</li>
+        </Link>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
