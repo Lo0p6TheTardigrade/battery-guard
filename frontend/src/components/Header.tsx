67129383146a8dbfccfb613724a7ff6ca2b1783e
Lo0p6TheTardigrade <rezonance30@gmail.com>
2024-04-06 04:23:09 +0200

    Add logo in Header.tsx component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Img from './Img';
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      Header
+      <Img
+        src="https://res.cloudinary.com/dp5onh4yw/image/upload/v1712369914/assets/images/BatteryGuardLogoB_da04hb.webp"
+        alt="logo du site"
+        classX="logo"
+        height={90}
+        width={210}
+      />
+    </header>
+  );
 };
 
 export default Header;
